350965ba4676571829a010861fcaa6ece3bb23c9
Joe Sweeney <joe.sweeney224@gmail.com>
2018-06-05 01:02:30 -0600

    Fiddling around

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.4.3/dist/css/foundation.min.css" integrity="sha256-GSio8qamaXapM8Fq9JYdGNTvk/dgs+cMLgPeevOYEx0= sha384-wAweiGTn38CY2DSwAaEffed6iMeflc0FMiuptanbN4J+ib+342gKGpvYRWubPd/+ sha512-QHEb6jOC8SaGTmYmGU19u2FhIfeG+t/hSacIWPpDzOp5yygnthL3JwnilM7LM1dOAbJv62R+/FICfsrKUqv4Gg==" crossorigin="anonymous">
     <title>Bozeman Laravel Todos</title>
 </head>
 <body>
-    <h1 class="text-center">Bozeman Laravel Todos</h1>
-
+    <!-- <h1 class="text-center">Bozeman Laravel Todos</h1> -->
+    <!-- <div class="grid-container">
+        <div class="grid-x">
+            <div class="cell small-12 medium-6 medium-offset-3">
+                <ul class="menu vertical">
+                    <li><input id="checkbox4" type="checkbox"><label for="checkbox4">Checkbox 1</label></li>
+                    <li><input id="checkbox5" type="checkbox"><label for="checkbox5">Checkbox 2</label></li>
+                    <li><input id="checkbox6" type="checkbox"><label for="checkbox6">Checkbox 3</label></li>
+                </ul>
+            </div>
+        </div>
+    </div> -->
     <!-- Compressed JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.4.3/dist/js/foundation.min.js" integrity="sha256-mRYlCu5EG+ouD07WxLF8v4ZAZYCA6WrmdIXyn1Bv9Vk= sha384-KzKofw4qqetd3kvuQ5AdapWPqV1ZI+CnfyfEwZQgPk8poOLWaabfgJOfmW7uI+AV sha512-0gHfaMkY+Do568TgjJC2iMAV0dQlY4NqbeZ4pr9lVUTXQzKu8qceyd6wg/3Uql9qA2+3X5NHv3IMb05wb387rA==" crossorigin="anonymous"></script>
 </body>
